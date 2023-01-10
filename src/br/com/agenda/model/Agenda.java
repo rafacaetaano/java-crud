f836b4c1310d1ff12582eee5247ab376f39882ba
rafacaetaano <you@example.com>
2023-01-10 19:05:53 -0300

    primeiro commit do projeto CRUD em Java

---
+++ src/br/com/agenda/model/Agenda.java
@@ -0,0 +1,37 @@
+package br.com.agenda.model;
+
+import java.util.Date;
+
+public class Agenda {
+	
+	private int id;
+	private String nome;
+	private int idade;
+	private Date data;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+}
